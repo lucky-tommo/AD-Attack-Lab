efda90a94b9c92547afbbafad1e35bd96ee4f571
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-05-09 11:58:43 +1000

    Create BBschema.ps1

---
+++ BBschema.ps1
@@ -0,0 +1,82 @@
+{
+    "domain": "luckyspider.org",
+
+    "groups" : [
+        {
+            "name": "Employees",
+            "name": "IT Admins",
+            "name": "Helpdesk",
+            "name": "Accounting"
+        }
+    ],
+
+    "users": [
+
+        {
+            "local_admin": ["ws01"],
+            "name": "Skyler White",
+            "password":"ilovewalt2024",
+            "groups": [
+                "Employees",
+                "Accounting"
+            ]
+        },
+        {           
+            "name": "Walter White",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees"
+            ]
+        },
+        {
+            "name": "Jesse Pinkman",
+            "password":"yehscienceB",
+            "groups": [
+                "Employees"
+            ]
+        {
+            "name": "Gus Fring",
+            "password":"chickenman2024",
+            "groups": [
+                "Employees"
+            ]
+        }
+        {
+            "name": "Hank Schrader",
+            "password":"youretoostupidtosee",
+            "groups": [
+                "Employees"
+            ]
+        }
+        {
+            "name": "Saul Goodman",
+            "password":"s'allgoodman",
+            "groups": [
+                "Employees"
+            ]
+        }
+        {
+            "name": "adm.Walter White",
+            "password":"iamtheonewhoknocks",
+            "groups": [
+                "IT Admin"
+            ]
+        }
+        {
+            "name": "adm.Jesse Pinkman",
+            "password":"yehscienceB",
+            "groups": [
+                "IT Admin"
+            ]
+        }
+        {
+            "name": "adm.Gus Fring",
+            "password":"lookatmehector",
+            "groups": [
+                "IT Admin"
+            ]
+        }
+
+        }
+    ]
+}
