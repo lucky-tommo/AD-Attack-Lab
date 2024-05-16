aa12c31edea7ad8f281deb7e3a95c7a8e154eab4
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-05-16 15:07:32 +1000

    Update BBschema.ps1

--- BBschema.ps1
+++ BBschema.ps1
@@ -62,21 +62,24 @@
             "name": "Jesse Pinkman",
             "password":"yehscienceB",
             "groups": [
-                "Employees"
+                "Employees",
+                "OG Crew"
             ]
         },
         {
             "name": "Gus Fring",
             "password":"chickenman2024",
             "groups": [
-                "Employees"
+                "Employees",
+                "ChickenCrew"
             ]
         },
         {
             "name": "Hank Schrader",
             "password":"youretoostupidtosee",
             "groups": [
-                "Employees"
+                "Employees",
+                "DEA"
             ]
         },
         {
@@ -91,21 +94,158 @@
             "name": "adm.Walter White",
             "password":"iamtheonewhoknocks",
             "groups": [
-                "IT Admin"
+                "IT Admin",
+                "Super Secret Super Lab"
             ]
         },
         {
             "name": "adm.Jesse Pinkman",
             "password":"yehscienceB",
             "groups": [
-                "IT Admin"
+                "IT Admin",
+                "Super Secret Super Lab"
             ]
         },
         {
             "name": "adm.Gus Fring",
             "password":"lookatmehector",
             "groups": [
-                "IT Admin"
+                "IT Admin",
+                "Super Secret Super Lab"
+            ]
+        },
+        {           
+            "name": "Marie Schrader",
+            "password":"P@$$W0rd",
+            "groups": [
+                "Employees",
+                "DEA",
+                "HR"
+            ]
+        },
+        {           
+            "name": "Walter WhiteJR",
+            "password":"P@$$w0rd",
+            "groups": [
+                "Employees",
+                "HR"
+            ]
+        },
+        {           
+            "name": "Mike Ehrmantraut",
+            "password":"JackofAllTrades2024",
+            "groups": [
+                "Employees",
+                "Cartel",
+                "ChickenCrew",
+                "Vamonos Pest"
+            ]
+        },
+        {           
+            "name": "Tuco Salamanca",
+            "password":"P@$$w0rd",
+            "groups": [
+                "Employees",
+                "Cartel"
+            ]
+        },
+        {           
+            "name": "Hector Salamanca",
+            "password":"P@$$w0rd",
+            "groups": [
+                "Employees",
+                "Cartel"
+            ]
+        },
+        {           
+            "name": "Gale Boetticher",
+            "password":"P@$$w0rd",
+            "groups": [
+                "Employees",
+                "ChickenCrew",
+                "Super Secret Super Lab"
+            ]
+        },
+        {           
+            "name": "Leonel Salamanca",
+            "password":"P@$$w0rd",
+            "groups": [
+                "Employees",
+                "Cartel"
+            ]
+        },
+        {           
+            "name": "Marco Salamanca",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "Cartel"
+            ]
+        },
+        {           
+            "name": "Nacho Varga",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "Cartel"
+            ]
+        },
+        {           
+            "name": "Lydia Rodarte-Quayle",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "Madrigal Engineering"
+            ]
+        },
+        {           
+            "name": "Ron Forenail",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "Madrigal Engineer"
+            ]
+        },
+        {           
+            "name": "Todd Alquist",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "Vamonos Pest"
+            ]
+        },
+        {           
+            "name": "Krazy Eight",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "Cartel",
+                "DEA"
+            ]
+        },
+        {           
+            "name": "Andrea Cantillo",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "OG Crew",
+                "HR"
+            ]
+        },
+        {           
+            "name": "Brandon Mayhew",
+            "password":"IAMTHEBADGER",
+            "groups": [
+                "Employees",
+                "OG Crew"
+            ]
+        },
+        {           
+            "name": "Skinny Pete",
+            "password":"iamthedanger",
+            "groups": [
+                "Employees",
+                "OG Crew"
             ]
         }
 
