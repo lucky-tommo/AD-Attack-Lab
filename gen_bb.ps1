7b8d4fbed7b0021818e0751efdfe236bb4bbc05b
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-05-16 13:33:48 +1000

    Update gen_bb.ps1

--- gen_bb.ps1
+++ gen_bb.ps1
@@ -40,11 +40,17 @@ function CreateADUser(){
     $password = $userObject.password
 
     # Generate a "first initial, last name" structure for username
+    #$firstname, $lastname = $name.Split(" ")
+    #$username = ($firstname[0] + $lastname).ToLower()
+    #$samAccountName = $username
+    #$principalname = $username
+
+    #Generate a "first name, last name" structure for username - user adm.firstname for admin accounts in JSON file. 
     $firstname, $lastname = $name.Split(" ")
-    $username = ($firstname[0] + $lastname).ToLower()
+    $username = ($firstname + $lastname).ToLower()
     $samAccountName = $username
     $principalname = $username
-
+    
     # Actually create the AD user object
     New-ADUser -Name "$name" -GivenName $firstname -Surname $lastname -SamAccountName $SamAccountName -UserPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -PassThru | Enable-ADAccount
 
