786a45c5675f40e75935354d55b45317d3cbfd41
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-05-16 18:15:04 +1000

    Update setDC1.ps1

--- setDC1.ps1
+++ setDC1.ps1
@@ -3,4 +3,4 @@ New-NetIPAddress -IPAddress 172.27.0.200 -DefaultGateway 172.27.0.2 -PrefixLengt
 Set-DNSClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses 172.27.0.200, 172.27.0.2
 Install-windowsfeature AD-domain-services
 Import-Module ADDSDeployment
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\\Windows\\NTDS" -DomainMode "7" -DomainName "luckyspider.org" -DomainNetbiosName "luckyspider" -ForestMode "7" -InstallDns:$true -LogPath "C:\\Windows\\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\\Windows\\SYSVOL" -Force:$true
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\\Windows\\NTDS" -DomainMode "7" -DomainName "breakingbad.abq" -DomainNetbiosName "breakingbad" -ForestMode "7" -InstallDns:$true -LogPath "C:\\Windows\\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\\Windows\\SYSVOL" -Force:$true
