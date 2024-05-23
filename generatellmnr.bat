b5f475c7e39a197cd94dd4d139a1ef1c36bd5f9d
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-05-23 11:12:03 +1000

    Update generatellmnr.bat

--- generatellmnr.bat
+++ generatellmnr.bat
@@ -1,8 +1,17 @@
-#run at login - does the startup folder still work? 
-
-:check_if_exists
-if exist H:\BackupConfig.txt goto perform
+#basic version to have all machines generating LLMNR traffic every X amount of time (configured by timeout) 
+:Generate_LLMNR
+pushd \\Filesevrer\
 timeout /t 30 /nobreak
-:perform
-pushd \\itsatrap\nothinghere
-goto :check_if_exists 
+goto :Generate_LLMNR
+
+
+
+
+#This section below should operate in response to a file - Utilise icons on a share drive - e.g create BackupConfig.json with the icon set as \\anythingnotreal\icon.jpg - upload to share drive - every access of the share drive will trigger LLMNR
+#:check_if_exists
+#if exist H:\BackupConfig.json goto perform
+#timeout /t 30 /nobreak
+#:perform
+#set this to match the icon - this will simulate users accessing the share drive
+#pushd \\anythingnotreal\ 
+#goto :check_if_exists 
