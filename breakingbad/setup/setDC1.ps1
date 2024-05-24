b103a30540ede946bf49fe87df39c9bbcec54356
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-05-24 17:44:47 +1000

    Update setDC1.ps1

--- breakingbad/setup/setDC1.ps1
+++ breakingbad/setup/setDC1.ps1
@@ -1,6 +1,7 @@
 Rename-Computer "DC1"
 New-NetIPAddress -IPAddress 172.27.0.200 -DefaultGateway 172.27.0.2 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
 Set-DNSClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses 172.27.0.200, 172.27.0.2
-Install-windowsfeature AD-domain-services
+Install-windowsfeature AD-domain-services -IncludeManagementTools
 Import-Module ADDSDeployment
+Import-Module ActiveDirectory
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\\Windows\\NTDS" -DomainMode "7" -DomainName "breakingbad.abq" -DomainNetbiosName "breakingbad" -ForestMode "7" -InstallDns:$true -LogPath "C:\\Windows\\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\\Windows\\SYSVOL" -Force:$true
